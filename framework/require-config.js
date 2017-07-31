6aa30d69ac9921538abf78db43ce70ec5253b1f4
Xiao Shen <dead.xx@gmail.com>
2017-07-31 16:48:44 +0800

    no message

--- framework/require-config.js
+++ framework/require-config.js
@@ -1,13 +1,13 @@
 'use strict';
 ! function() {
 	//请确保modules第一个被赋值
-	var modules = {"common/kernel":3,"common/pointerevents":1,"common/svgicos":1,"common/touchguesture":1,"common/touchslider":1,"page/home":1,"page/list":1,"page/settings":1,"page/user":1,"popup/samplePopup":1,"popup/samplePopup2":1,"site/index":1,"site/pages":1,"site/popups":1},
+	var modules = {"common/kernel":4,"common/pointerevents":1,"common/svgicos":1,"common/touchguesture":1,"common/touchslider":1,"page/home":1,"page/list":1,"page/settings":1,"page/user":1,"popup/samplePopup":1,"popup/samplePopup2":1,"site/index":2,"site/pages":1,"site/popups":1},
 		//请确保srcRoot第二个被赋值
 		srcRoot = 'dev/',
 		//请确保productRoot第三个被赋值
 		productRoot = 'dist/',
 		//请确保siteVersion第四个被赋值
-		siteVersion = "1.0.264",
+		siteVersion = "1.0.265",
 		//请确保debug第五个被赋值
 		debug = false,
 		prefix = document.currentScript.src.replace(/^http(s)?:\/\/[^\/]+|framework\/[^\/]+$/g, ''),
