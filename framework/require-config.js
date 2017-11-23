49f50ba8a6a0feefca575567e06696cca423e836
Xiao Shen <dead.xx@gmail.com>
2017-11-23 16:30:56 +0800

    no message

--- framework/require-config.js
+++ framework/require-config.js
@@ -1,13 +1,13 @@
 'use strict';
 ! function() {
 	//请确保modules第一个被赋值
-	var modules = {"common/kernel":8,"common/pointerevents":3,"common/svgicos":3,"common/touchguesture":2,"common/touchslider":3,"page/list":4,"page/settings":2,"page/user":2,"popup/page":2,"popup/samplePopup":2,"popup/samplePopup2":2,"site/apis":2,"site/index":5,"site/pages":2,"site/popups":3},
+	var modules = {"common/kernel":9,"common/pointerevents":3,"common/svgicos":3,"common/touchguesture":2,"common/touchslider":3,"page/list":4,"page/settings":2,"page/user":2,"popup/page":2,"popup/samplePopup":2,"popup/samplePopup2":2,"site/apis":2,"site/index":5,"site/pages":2,"site/popups":3},
 		//请确保srcRoot第二个被赋值
 		srcRoot = 'dev/',
 		//请确保productRoot第三个被赋值
 		productRoot = 'dist/',
 		//请确保siteVersion第四个被赋值
-		siteVersion = "1.0.271",
+		siteVersion = "1.0.272",
 		//请确保debug第五个被赋值
 		debug = false,
 		prefix = document.currentScript.src.replace(/^http(s)?:\/\/[^\/]+|framework\/[^\/]+$/g, ''),
