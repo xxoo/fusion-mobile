23dff6d59b465070429ea8f6a923206e24277cbe
沈啸 <dead.xx@gmail.com>
2018-09-28 01:01:44 +0800

    no message

--- framework/combine.cmd
+++ framework/combine.cmd
@@ -1,2 +1,2 @@
 cd /d %~dp0
-uglifyjs src\qsa-scope.js src\svgClassList.js src\require.js src\browser.js src\init.js -c hoist_vars,unsafe,comparisons -m -o all.js
+terser src\qsa-scope.js src\svgClassList.js src\require.js src\browser.js src\init.js -c hoist_vars,unsafe,comparisons -m -o all.js
