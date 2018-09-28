7200a46faf27998c98965b08ce39746f8f63b52e
沈啸 <dead.xx@gmail.com>
2018-09-28 13:49:50 +0800

    no message

--- dist/site/pages/1/pages.js
+++
@@ -1,1 +0,0 @@
-"use strict";define({home:{js:"home",html:"home.html",css:"home.less",title:"首页"},list:{js:"list",html:"list.html",css:"list.less",title:"列表"},user:{html:"user.html",css:"user.less",title:"用户中心",back:"list"},settings:{js:"settings",html:"settings.html",css:"settings.less",title:"设置",back:"user"}});
